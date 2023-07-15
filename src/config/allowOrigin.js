4cf7bf2ed8441a91aae23cdab3b629a776a3d807
kebzin <honorablewaiga@gmail.com>
2023-07-15 16:14:31 +0000

    change allow origin

--- src/config/allowOrigin.js
+++ src/config/allowOrigin.js
@@ -1,5 +1,8 @@
 // This file list the allow the origin that alre allow to acces our api
 
-const allowOrigin = ["http://192.168.94.44:19000", "http://localhost:3000"];
+const allowOrigin = [
+  "https://venchfindsystem.onrender.com/",
+  "http://localhost:3000",
+];
 
 module.exports = allowOrigin;
