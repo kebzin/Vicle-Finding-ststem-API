5aeb2f9b54ead29d00131adc4acf00513d815362
kebzin <honorablewaiga@gmail.com>
2023-07-15 17:31:05 +0000

    allow org

--- src/config/allowOrigin.js
+++ src/config/allowOrigin.js
@@ -1,7 +1,7 @@
 // This file list the allow the origin that alre allow to acces our api
 
 const allowOrigin = [
-  "https://venchfindsystem.onrender.com",
+  "https://venchfindsystem-1l9s.onrender.com",
   "http://localhost:3000",
 ];
 
